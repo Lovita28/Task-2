2012ee5147ff8376124343e662129f2b302b670d
Lovita28 <lovitaanjani@gmail.com>
2017-07-26 14:31:45 +0700

    Tugas 2 Evaluasi dan Validasi

---
+++ Task 2 Validasi angka huruf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_Validasi_angka_huruf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int angka;
+
+            Console.Write("Input apa saja :");
+            a = Console.ReadLine();
+
+            if (int.TryParse(a, out angka))
+            {
+                Console.WriteLine("angka");
+            }
+            else
+            {
+                Console.WriteLine("huruf");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Tugas 2 Evaluasi Nilai/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_2_Evaluasi_Nilai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Input your value :");
+            a = int.Parse(Console.ReadLine());
+
+            if (a > 80)
+            {
+                Console.WriteLine("Your grade is A");
+
+            }
+            else if (a >= 65 && a <= 80)
+            {
+                Console.WriteLine(" Your grade is B");
+            }
+            else if (a >= 45 && a < 65)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (a >= 30 && a < 40)
+            {
+                Console.WriteLine("Your grade is D");
+            }
+            else if (a < 30)
+            {
+                Console.WriteLine("Your grade is E");
+            }
+
+            Console.Read();
+        }
+    }
+}
